10dca906c87fa3db21b27289d388748099185abf
Adam Rehn <adam@adamrehn.com>
2022-04-06 18:32:10 +1000

    Add entrypoint script to fix PulseAudio container restart issues

---
+++ pulseaudio/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Remove any PulseAudio files leftover from previous runs of the Unreal Engine
+# (Without this, PulseAudio will hang on startup if a container is restarted without a sufficient shutdown grace period)
+rm -rf ~/.config/pulse /tmp/pulse-*
+
+# Run the entrypoint command specified via our command-line parameters
+$*
